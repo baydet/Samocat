95f91059fa2da37c700e67e595704fefa6abaf13
Orangesoft mini <orangesoftmacmini@Mac-mini-Orangesoft-2.local>
2017-04-14 12:05:20 +0300

    Initial Commit

---
+++ Samocut/Samocut.h
@@ -0,0 +1,19 @@
+//
+//  Samocut.h
+//  Samocut
+//
+//  Created by Orangesoft Developer on 14.04.17.
+//  Copyright © 2017 Orangesoft Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Samocut.
+FOUNDATION_EXPORT double SamocutVersionNumber;
+
+//! Project version string for Samocut.
+FOUNDATION_EXPORT const unsigned char SamocutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Samocut/PublicHeader.h>
+
+
